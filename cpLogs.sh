a6a0237b5487581d1212aa4fa432a7ce71a5fe96
bogmart <36724163+bogmart@users.noreply.github.com>
2023-09-13 13:34:25 +0300

    copy system logs

--- cpLogs.sh
+++ cpLogs.sh
@@ -7,3 +7,8 @@ mv /tmp/myLogs/logWl.txt /jffs/myLogs/logWl_$(date +%y.%m.%d_%a).txt
 cp -p  /tmp/myLogs/*  /jffs/myLogs/
 rm /tmp/myLogs/*
 
+#copy system logs
+cp /var/log/messages  /jffs/myLogs/var_logs_$(date +%y.%m.%d_%a).txt
+
+
+
